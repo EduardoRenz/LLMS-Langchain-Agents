ee09c602909b6df01007df1baa9f0669f74ad77b
Eduardo Renz <dudarenz@gmail.com>
2024-10-22 23:21:10 -0300

    small improvements

--- web_scraper.py
+++ web_scraper.py
@@ -1,26 +1,25 @@
 # %%
 from langchain_community.document_loaders import AsyncChromiumLoader
-from langchain_community.document_transformers import Html2TextTransformer
+from langchain_community.document_transformers import BeautifulSoupTransformer
 from langchain.prompts import PromptTemplate
 from llm_factory import create_model
 import nest_asyncio
 # %%
 llm = create_model()
 nest_asyncio.apply()
-url = "https://www.sefaz.rs.gov.br/ASP/AAE_ROOT/NFE/SAT-WEB-NFE-NFC_2.asp?HML=false&chaveNFe=43240313891196000101650010000859501401545934"
+url = "https://www.sefaz.rs.gov.br/ASP/AAE_ROOT/NFE/SAT-WEB-NFE-NFC_2.asp?HML=false&chaveNFe=43240202721998000149650030000836801447941365"
 
 loader = AsyncChromiumLoader(
     [url], user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.142.86 Safari/537.36")
 docs = loader.load()
-tt = Html2TextTransformer()
-fd = tt.transform_documents(docs)
+transformer = BeautifulSoupTransformer()
+fd = transformer.transform_documents(docs, tags_to_extract=["table"])
 
 # %%
 prompt = PromptTemplate.from_template(
-    """Extraia apenas a tabela de itens de mercado, preco e quantidade e transforme em csv o seguinte input nao diga mais nada, apenas extria o csv puro pronto para ser jogado em um arquivo, sem por markdown: 
+    """Extraia apenas a tabela de itens de mercado, preco e quantidade e transforme em csv o seguinte input nao diga mais nada, apenas extria o csv puro pronto para ser jogado em um arquivo. 
 
     Adicione mais duas colunas no final, descricao reduzida e categoria, e preencha as
-
     As categorias disponiveis sao:
         -  alimentos
         -  bebidas
@@ -28,20 +27,29 @@ prompt = PromptTemplate.from_template(
         -  higiene
         -  outros
         -  utensilhos
+
+    A descricao reduzida deve ser em minusculo, sem acentos
+
+    Nao coloque nenhum tipo de markdown, o csv tem que ser puro e pronto para uso
+    Segue o input:
     --------
     {input}
     -------
     """
 )
 
-
 chain = prompt | llm
-# %%
 
-result = chain.invoke({"input": fd[0].page_content})
 # %%
+result = chain.invoke({"input": fd[0].page_content})
+
+# %%
+cleaned_result = result.content.replace(',', '.').replace(';', ',')
 # Create a csv file
 with open('output.csv', 'w') as f:
-    f.write(result.content)
+    f.write(cleaned_result)
 # %%
-print(result.content)
+print(result)
+
+# %%
+llm.invoke("Qual foi a ultima palavra dita por voce?")
